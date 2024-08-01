d46156ccea561fe8cb90f5fc51c6f275d233854e
Ianto Lin Xi <iantolinxi@gmail.com>
2024-08-01 19:41:29 -0400

    more stuff

--- Math2001/04_Proofs_with_Structure_II/03_Exists_Unique.lean
+++ Math2001/04_Proofs_with_Structure_II/03_Exists_Unique.lean
@@ -24,14 +24,49 @@ example : ∃! x : ℚ, ∀ a, a ≥ 1 → a ≤ 3 → (a - x) ^ 2 ≤ 1 := by
   use 2
   dsimp
   constructor
-  · intro y h1 h3
-    have h5: (y - 2)^2 =
+  · intro y h1 h2
+    have h3: y - 2 ≥ -1 := by
+      calc
+      y - 2 ≥ 1 - 2 := by rel [h1]
+      _ = -1 := by ring
+    have h4: y - 2 ≤ 1 := by
+      calc
+      y - 2 ≤ 3 - 2 := by rel [h2]
+      _ = 1 := by ring
+    have h5: (y - 2)^2 ≤ 1^2 := by
+      apply sq_le_sq'
+      use h3
+      use h4
+    have h6: (y - 2)^2 ≤ 1 := by
+      calc
+      (y-2)^2 ≤ 1^2 := by rel [h5]
+      _ = 1 := by ring
+    use h6
+  · intro y hy
+    have h1: (1 - y)^2 ≤ 1 := by
+      apply hy
+      numbers
+      numbers
+    have h2: (3 - y)^2 ≤ 1 := by
+      apply hy
+      numbers
+      numbers
+    have h3: (y-2)^2 ≤ 0 := by
+      calc
+      (y - 2)^2 = ((1 - y)^2 + (3 - y)^2 - 2) / 2 := by ring
+      _ ≤ (1 + (3 - y)^2 - 2) / 2 := by rel [h1]
+      _ ≤ (1 + 1 - 2) / 2 := by rel [h2]
+      _ = 0 := by ring
+    have h4: (y-2)^2 = 0 := by
+      apply le_antisymm
+      · exact h3
+      · apply pow_two_nonneg
+    cancel 2 at h4
+    calc
+      y = y - 0 := by ring
+      _ = y - (y - 2) := by rw [h4]
+      _ = 2 := by ring
 
-    rw sq_le_sq'
-
-
-
-  sorry
 
 example {x : ℚ} (hx : ∃! a : ℚ, a ^ 2 = x) : x = 0 := by
   obtain ⟨a, ha1, ha2⟩ := hx
@@ -80,10 +115,27 @@ example : ∃! r : ℤ, 0 ≤ r ∧ r < 5 ∧ 14 ≡ r [ZMOD 5] := by
 
 
 example : ∃! x : ℚ, 4 * x - 3 = 9 := by
-  sorry
+  use 3
+  dsimp
+  constructor
+  · numbers
+  · intro r hr
+    have h1 :=
+      calc
+      4 * r = (4 * r - 3) + 3 := by ring
+      _ = 9 + 3 := by rw [hr]
+      _ = 4 * 3 := by ring
+    cancel 4 at h1
 
 example : ∃! n : ℕ, ∀ a, n ≤ a := by
-  sorry
+  use 0
+  dsimp
+  constructor
+  · intro r
+    exact Nat.zero_le r
+  · intro m hm
+    specialize hm 0
+    exact Nat.eq_zero_of_le_zero hm
 
 example : ∃! r : ℤ, 0 ≤ r ∧ r < 3 ∧ 11 ≡ r [ZMOD 3] := by
   sorry
